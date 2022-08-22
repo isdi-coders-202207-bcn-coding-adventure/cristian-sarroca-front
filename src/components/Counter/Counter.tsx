2753650b56845e2045a4708a0ff29709962e7d7d
CSarroca <sarroca93@gmail.com>
2022-08-22 15:26:51 +0200

    Created Counter component with his html

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <ul className="counter">
+      <li className="counter__days">03</li>
+      <li className="counter__hours">22</li>
+      <li className="counter__minutes">50</li>
+      <li className="counter__seconds">44</li>
+    </ul>
+  );
+};
+export default Counter;
