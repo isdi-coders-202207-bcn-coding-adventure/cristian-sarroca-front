e1062213cd7e491990e9dc1b9734c8e71fa03bc5
CSarroca <sarroca93@gmail.com>
2022-08-23 13:02:50 +0200

    Added anidation at styledCounter

--- src/components/Counter/StyledCounter.tsx
+++ src/components/Counter/StyledCounter.tsx
@@ -2,34 +2,39 @@ import styled from "styled-components";
 import "@fontsource/oxanium";
 
 const StyledCounter = styled.ul`
+  height: 80vh;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   list-style: none;
   margin: 0;
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
+
   font-family: "oxanium", cursive;
-  gap: 5px;
+  gap: 25px;
 
   .counter-input {
     display: flex;
     flex-direction: column;
-  }
-
-  .counter-input__digit {
-    background-color: #ffc700;
-    border-radius: 20%;
-    font-size: 3em;
-    width: 150px;
-    height: 150px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .counter-input__text {
-    color: white;
-    font-size: 2em;
-    align-self: center;
+    &__digit {
+      background-color: #ffc700;
+      border-radius: 20%;
+      font-size: 3em;
+      width: 150px;
+      height: 150px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    &__text {
+      color: white;
+      font-size: 2em;
+      align-self: center;
+    }
   }
 `;
 
