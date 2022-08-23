9f45a0948215dee263b86c8bcb72e38c5046d6a3
CSarroca <sarroca93@gmail.com>
2022-08-23 15:45:53 +0200

    Added logical for Counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,22 +1,48 @@
 import StyledCounter from "./StyledCounter";
+import { useEffect, useState } from "react";
+import limitDate from "../../data/limitDate";
 
 const Counter = () => {
+  const initialState = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+  const [timeLeft, setTimeLeft] = useState(initialState);
+
+  const calculateTimeLeft = () => {
+    const difference = +new Date(limitDate) - +new Date();
+    const timeCalculated = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      ),
+      minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((difference % (1000 * 60)) / 1000),
+    };
+
+    setTimeLeft(timeCalculated);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      calculateTimeLeft();
+    }, 1000);
+  });
+
   return (
     <StyledCounter>
       <li className="counter-input">
-        <div className="counter-input__digit">02</div>
+        <div className="counter-input__digit">{timeLeft.days}</div>
         <div className="counter-input__text">days</div>
       </li>
       <li className="counter-input">
-        <div className="counter-input__digit">20</div>
+        <div className="counter-input__digit">{timeLeft.hours}</div>
         <div className="counter-input__text">hours</div>
       </li>
       <li className="counter-input">
-        <div className="counter-input__digit">34</div>
+        <div className="counter-input__digit">{timeLeft.minutes}</div>
         <div className="counter-input__text">minutes</div>
       </li>
       <li className="counter-input">
-        <div className="counter-input__digit">09</div>
+        <div className="counter-input__digit">{timeLeft.seconds}</div>
         <div className="counter-input__text">seconds</div>
       </li>
     </StyledCounter>
---
+++ src/data/limitDate.ts
@@ -0,0 +1,3 @@
+const limitDate = "2022-09-28T18:30:00+05:30";
+
+export default limitDate;
